a64502f1357bbca5f2b8be16f45272c76d871dfb
王正东 <wangzhengdong@ecmoban.com>
2020-11-17 18:07:21 +0800

    更新配置文件

--- config/rpc-services.php
+++ config/rpc-services.php
@@ -12,5 +12,12 @@ return [
                 ['host' => '127.0.0.1', 'port' => 9503, 'path' => '/rpc']
             ]
         ]
-    ]
+    ],
+
+    // auth user
+    'auth_user' => env('RPC_AUTH_USER'),
+
+    // auth password
+    'auth_password' => env('RPC_AUTH_PASSWORD'),
+
 ];
